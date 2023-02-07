a74c9eb033544b7424522786ef943a67c5805cf3
Manuel Vega <manuelvega.dev@gmail.com>
2023-02-07 15:15:22 -0500

    fix(project): Disabling the experimental 'app' directory of NextJS by errors in console

--- app/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Yee - Control de empleados</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-    </>
-  )
-}
